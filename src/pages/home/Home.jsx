105f2dcaffd2cd45ac1a4961f0241a83e57bc51e
Siddhant Sharma <siddhantsharma909@gmail.com>
2023-10-25 23:55:35 +0530

    ADD : Animation Added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import styles from "./home.module.css"
+import { COLORS } from '../../themes/colors'
 
 const Home = () => {
   return (
     <>
-      <h1 className={styles.headertitle}>Easy Rev</h1>
+      <div className={styles.box} ></div>
+      <div className={styles.box1} ></div>
+      <div className={styles.box2} ></div>
+      <div className={styles.box3} ></div>
+      <div className={styles.box4} ></div>
+      <div className={styles.topFeedContainer}>
+        <div className={styles.topInnerContainer}>
+          <h3 style={{ color: COLORS.TEXTCOLOR }} >Easy Rev</h3>
+          <p style={{ color: COLORS.TEXTCOLOR }}>
+            Easy Rev is a review card for business owners, making it convenient for business owners to collect reviews and insights from customers.
+          </p>
+        </div>
+      </div>
     </>
   )
 }
