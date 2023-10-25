b824815fffa051edaee7a5c3415dc2abae2187dc
Siddhant Sharma <siddhantsharma909@gmail.com>
2023-10-25 22:57:51 +0530

    ADD : Font family and project structure added

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./home.module.css"
+
+const Home = () => {
+  return (
+    <>
+      <h1 className={styles.headertitle}>Easy Rev</h1>
+    </>
+  )
+}
+
+export default Home
---
+++ src/themes/colors.js
@@ -0,0 +1,7 @@
+export const COLORS = {
+    PRIMARY : "#FFC72C",
+    SECONDARY : "#2c64ff",
+    TERTIARY : "#DB4437",
+    QUATERNARY : "#000000",
+    TEXTCOLOR : "#0E3846"
+}
